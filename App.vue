094d3b7e01ce8f1c0a7637a966be2ddf926c9072
Your Name <you@example.com>
2019-06-29 08:46:37 +0800

    表单校验完成

--- App.vue
+++ App.vue
@@ -1,119 +1,66 @@
 <template>
-  <div id="app">
-    <routr-view/>
-
-    <el-form
-      :model="ruleForm"
-      :rules="rules"
-      ref="ruleForm"
-      label-width="100px"
-      class="demo-ruleForm"
-    >
-      <el-form-item label="活动名称" prop="name">
-        <el-input v-model="ruleForm.name"></el-input>
-      </el-form-item>
-      <el-form-item label="活动区域" prop="region">
-        <el-select v-model="ruleForm.region" placeholder="请选择活动区域">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="活动时间" required>
-        <el-col :span="11">
-          <el-form-item prop="date1">
-            <el-date-picker
-              type="date"
-              placeholder="选择日期"
-              v-model="ruleForm.date1"
-              style="width: 100%;"
-            ></el-date-picker>
-          </el-form-item>
-        </el-col>
-        <el-col class="line" :span="2">-</el-col>
-        <el-col :span="11">
-          <el-form-item prop="date2">
-            <el-time-picker placeholder="选择时间" v-model="ruleForm.date2" style="width: 100%;"></el-time-picker>
-          </el-form-item>
-        </el-col>
-      </el-form-item>
-      <el-form-item label="即时配送" prop="delivery">
-        <el-switch v-model="ruleForm.delivery"></el-switch>
-      </el-form-item>
-      <el-form-item label="活动性质" prop="type">
-        <el-checkbox-group v-model="ruleForm.type">
-          <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-          <el-checkbox label="地推活动" name="type"></el-checkbox>
-          <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-          <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="特殊资源" prop="resource">
-        <el-radio-group v-model="ruleForm.resource">
-          <el-radio label="线上品牌商赞助"></el-radio>
-          <el-radio label="线下场地免费"></el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="活动形式" prop="desc">
-        <el-input type="textarea" v-model="ruleForm.desc"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
-        <el-button @click="resetForm('ruleForm')">重置</el-button>
-      </el-form-item>
-    </el-form>
-  </div>
+  <el-row type="flex" class="row-bg" justify="center" align="middle">
+    <el-col :xs="14" :sm="12" :md="10" :lg="8" :xl="6">
+      <el-form
+        :model="ruleForm"
+        :rules="formRules"
+        ref="loginForm"
+        label-width="80px"
+        class="demo-ruleForm login-form"
+        label-position="top"
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="ruleForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="ruleForm.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('loginForm')">登录</el-button>
+          <el-button @click="resetForm('loginForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
       ruleForm: {
-        name: "",
-        region: "",
-        date1: "",
-        date2: "",
-        delivery: false,
-        type: [],
-        resource: "",
-        desc: ""
+        username: "admin",
+        password: "123456"
       },
-      rules: {
-        name: [
-          { required: true, message: "请输入活动名称", trigger: "blur" },
-          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
-        ],
-        region: [
-          { required: true, message: "请选择活动区域", trigger: "change" }
-        ],
-        date1: [
+      formRules: {
+        username: [
           {
-            type: "date",
             required: true,
-            message: "请选择日期",
+            message: "请输入用户名",
+            trigger: "blur"
+          },
+          {
+            min: 5,
+            max: 12,
+            message: "用户名必须是5个到12个字符",
             trigger: "change"
           }
         ],
-        date2: [
+        password: [
           {
-            type: "date",
             required: true,
-            message: "请选择时间",
+            message: "请输入密码",
+            trigger: "blur"
+          },
+          {
+            min: 6,
+            max: 15,
+            message: "密码必须是6个到15个字符",
             trigger: "change"
           }
-        ],
-        type: [
-          {
-            type: "array",
-            required: true,
-            message: "请至少选择一个活动性质",
-            trigger: "change"
-          }
-        ],
-        resource: [
-          { required: true, message: "请选择活动资源", trigger: "change" }
-        ],
-        desc: [{ required: true, message: "请填写活动形式", trigger: "blur" }]
+        ]
       }
     };
   },
@@ -121,9 +68,20 @@ export default {
     submitForm(formName) {
       this.$refs[formName].validate(valid => {
         if (valid) {
-          alert("submit!");
+          axios({
+            url: "http://localhost:8888/api/private/v1/login",
+            method: "post",
+            data: this.form
+            // 这里是嵌套解构
+          }).then(({ data: { data, meta } }) => {
+            if (meta.status == 200) {
+              localStorage.setItem("token", data.token);
+
+              this.$router.push("/home");
+            }
+          });
         } else {
-          console.log("error submit!!");
+          // console.log('error submit!!');
           return false;
         }
       });
@@ -134,3 +92,16 @@ export default {
   }
 };
 </script>
+<style>
+.row-bg {
+  height: 100%;
+  background-color: #2d434c;
+}
+.login-form {
+  background-color: #fff;
+  border-radius: 10px;
+  padding: 30px 20px;
+  min-width: 400px;
+}
+</style>
+
--- main.js
+++ main.js
@@ -3,10 +3,38 @@ import App from './App.vue'
 import router from './router'
 
 import ElementUI from 'element-ui'
-import 'element-ui/ lib/theme/index.css'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import './assets/css/index.css'
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
+
+router.beforeEach((to, from, next) => {
+  // 1. 判断用户访问的是不是登录页面， 如果是登录页面，直接放行
+  if (to.path === '/login') {
+    next()
+    return
+  }
+
+  // 2. 如果访问的是其他的页面，判断用户是否进行了登录
+  //    2.1 如果登录了，那就直接放行
+  //    2.2 如果没登录，那就跳转到登录页
+
+  if (localStorage.getItem('token')) {
+    next()
+  } else {
+    router.push('/login')
+  }
+
+  // console.log(to, from);
+  // console.log("导航守卫工作啦！！");
+  // next()
+
+  // next 这里提供的方法，是用来进行页面的跳转的
+  // 如果不调用这个方法，那么页面就不会进入
+  // next();
+})
 
 new Vue({
   router,
