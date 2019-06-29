24eacb8838d367afedd068c735c5d6cb9163b38d
Your Name <you@example.com>
2019-06-29 09:23:34 +0800

    修改到login

--- App.vue
+++ App.vue
@@ -1,107 +1,16 @@
 <template>
-  <el-row type="flex" class="row-bg" justify="center" align="middle">
-    <el-col :xs="14" :sm="12" :md="10" :lg="8" :xl="6">
-      <el-form
-        :model="ruleForm"
-        :rules="formRules"
-        ref="loginForm"
-        label-width="80px"
-        class="demo-ruleForm login-form"
-        label-position="top"
-      >
-        <el-form-item label="用户名" prop="username">
-          <el-input v-model="ruleForm.username"></el-input>
-        </el-form-item>
-        <el-form-item label="密码" prop="password">
-          <el-input v-model="ruleForm.password"></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="submitForm('loginForm')">登录</el-button>
-          <el-button @click="resetForm('loginForm')">重置</el-button>
-        </el-form-item>
-      </el-form>
-    </el-col>
-  </el-row>
+  <div id="app">
+    <router-view/>
+  </div>
 </template>
 
 <script>
-import axios from "axios";
-
-export default {
-  data() {
-    return {
-      ruleForm: {
-        username: "admin",
-        password: "123456"
-      },
-      formRules: {
-        username: [
-          {
-            required: true,
-            message: "请输入用户名",
-            trigger: "blur"
-          },
-          {
-            min: 5,
-            max: 12,
-            message: "用户名必须是5个到12个字符",
-            trigger: "change"
-          }
-        ],
-        password: [
-          {
-            required: true,
-            message: "请输入密码",
-            trigger: "blur"
-          },
-          {
-            min: 6,
-            max: 15,
-            message: "密码必须是6个到15个字符",
-            trigger: "change"
-          }
-        ]
-      }
-    };
-  },
-  methods: {
-    submitForm(formName) {
-      this.$refs[formName].validate(valid => {
-        if (valid) {
-          axios({
-            url: "http://localhost:8888/api/private/v1/login",
-            method: "post",
-            data: this.form
-            // 这里是嵌套解构
-          }).then(({ data: { data, meta } }) => {
-            if (meta.status == 200) {
-              localStorage.setItem("token", data.token);
-
-              this.$router.push("/home");
-            }
-          });
-        } else {
-          // console.log('error submit!!');
-          return false;
-        }
-      });
-    },
-    resetForm(formName) {
-      this.$refs[formName].resetFields();
-    }
-  }
-};
+export default {};
 </script>
+
 <style>
-.row-bg {
+#app {
   height: 100%;
-  background-color: #2d434c;
-}
-.login-form {
-  background-color: #fff;
-  border-radius: 10px;
-  padding: 30px 20px;
-  min-width: 400px;
 }
 </style>
 
